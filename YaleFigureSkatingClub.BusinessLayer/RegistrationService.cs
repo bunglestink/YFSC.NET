d4954318b8a48e468832490111996645fd5d27a8
Kirk Thompson <kwt104@gmail.com>
2012-04-14 17:03:59 -0400

    implement RegistrationService

--- YaleFigureSkatingClub.BusinessLayer/RegistrationService.cs
+++ YaleFigureSkatingClub.BusinessLayer/RegistrationService.cs
@@ -11,12 +11,61 @@ namespace YaleFigureSkatingClub.BusinessLayer
 		}
 		
 		
+		public void SubmitRegistration (AnnualRegistration registration) {
+			// TODO: implement this.
+		}
+		
 		public decimal GetRegistrationCost(AnnualRegistration registration)
+		{
+			var membershipCost = GetMembershipCost(registration);
+			var homeClubCost = GetHomeClubCost(registration);
+			var sessionCost = GetSessionCost(registration);
+		
+			return membershipCost + homeClubCost + sessionCost;
+		}
+		
+		decimal GetMembershipCost(AnnualRegistration registration)
+		{
+			// Yale Affiliated: $20/member
+			// Non-Yale: $30 first, $20 additional
+			var skaters = registration.Skaters.Count();
+			
+			return registration.YaleAffiliation ?
+				skaters * 20 :
+				skaters == 0 ? 
+					0 : 
+					(skaters * 20) + 10;
+		}
+		
+		decimal GetHomeClubCost(AnnualRegistration registration)
+		{
+			// Basic: $12 each, $12 for SnoplowSam parent
+			// Regular: $50 first, $20 additional
+			
+			decimal total = 0;
+			bool firstRegular = true;
+			
+			foreach (var skater in registration.Skaters) {
+				if (skater.Level != null && skater.Level.ToLower () == "basic") {
+					total += 12;
+				}
+				else if (firstRegular) {
+					total += 50;
+					firstRegular = false;
+				}
+				else {
+					total += 20;
+				}
+			}
+			
+			return total;
+		}
+		
+		decimal GetSessionCost(AnnualRegistration registration)
 		{
 			return registration.Skaters
 				.SelectMany(s => s.Sessions)
-				.Count();
+				.Sum(s => s.TotalCost);	
 		}
 	}
 }
-
